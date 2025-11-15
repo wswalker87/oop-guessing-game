f91c9319e574bc519c1b50042c700143454f18dd
wswalker87 <scott@wscottwalker.com>
2025-11-15 11:30:41 -0600

    initial commit

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,13 @@
+# let's do a game where you guess a number
+# give feedback on it the number is high, low, or corrent. 
+# create a method that returns true if the last guess was correct and false if not. 
+# bonus not in the directions: Keep a running count of games played and games won.
 class GuessingGame():
+
+
+    def __init__(self, user_guess):
+        pass
+
+
+    def user_input(self, user_guess):
+        user_guess = input("Guess a number between 1 and 50")
